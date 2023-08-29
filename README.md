c7cbda3213bb0c9ce468488a5f8357d04d8cfcef
Phodal Huang <h@phodal.com>
2023-08-29 14:20:46 +0800

    feat: init for README

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chocolate Factory
+
+> Chocolate Factory 是一款先进的 LLM 应用引擎，旨在帮助您打造自己的人工智能助手。
+
+Working process:
+
+1. DomainDetector. 
+2. QuestionAnalyzer.
+3. 
+3. QuestionClarifier. 
+4. SolutionExecutor. 
+
+## License
+
+This code is distributed under the MPL 2.0 license. See `LICENSE` in this directory.
--- src/main/kotlin/cc/unitmesh/cf/ChocolateFactoryApplication.kt
+++ src/main/kotlin/cc/unitmesh/cf/ChocolateFactoryApplication.kt
@@ -1,9 +1,11 @@
 package cc.unitmesh.cf
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ChocolateFactoryApplication
 
 fun main(args: Array<String>) {
