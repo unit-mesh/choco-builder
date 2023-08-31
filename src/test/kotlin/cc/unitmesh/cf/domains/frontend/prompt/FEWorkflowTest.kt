e1f614a2e7547a260a13cf50f9f17ce6f964e0a7
Phodal Huang <h@phodal.com>
2023-08-31 15:29:03 +0800

    fix: fix tes

--- src/test/kotlin/cc/unitmesh/cf/domains/frontend/prompt/FEWorkflowTest.kt
+++ src/test/kotlin/cc/unitmesh/cf/domains/frontend/prompt/FEWorkflowTest.kt
@@ -1,12 +1,13 @@
 package cc.unitmesh.cf.domains.frontend.prompt
 
+import cc.unitmesh.cf.core.prompt.PromptTemplate
 import org.junit.jupiter.api.Test
 
 class FEWorkflowTest {
 
     @Test
     fun should_return_pure_text_for_testing_in_gui() {
-        val template = FEWorkflow.CLARIFY.systemPrompt
+        val template = FEWorkflow().CLARIFY.systemPrompt
         println(template)
     }
 }
