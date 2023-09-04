f0dcdf9cfd9fcb36862590010857c737491bb486
Phodal Huang <h@phodal.com>
2023-09-04 14:22:45 +0800

    refactor: clean auth code

--- choc-ui/app/api/chat/route.ts
+++ choc-ui/app/api/chat/route.ts
@@ -1,7 +1,8 @@
 export const runtime = 'edge'
 
 export async function POST(req: Request) {
-  console.log(req);
+  console.log(req.body);
+
   return new Promise((resolve, reject) => {
     resolve(new Response('Hello from the API!'))
   })
--- choc-ui/app/chat/[id]/page.tsx
+++ choc-ui/app/chat/[id]/page.tsx
@@ -1,8 +1,5 @@
 import { type Metadata } from 'next'
-import { notFound, redirect } from 'next/navigation'
 
-import { auth } from '@/auth'
-import { getChat } from '@/app/actions'
 import { Chat } from '@/components/chat'
 
 export const runtime = 'edge'
@@ -17,34 +14,11 @@ export interface ChatPageProps {
 export async function generateMetadata({
   params
 }: ChatPageProps): Promise<Metadata> {
-  const session = await auth()
-
-  if (!session?.user) {
-    return {}
-  }
-
-  const chat = await getChat(params.id, session.user.id)
   return {
-    title: chat?.title.toString().slice(0, 50) ?? 'Chat'
+    title: 'Chat'
   }
 }
 
 export default async function ChatPage({ params }: ChatPageProps) {
-  const session = await auth()
-
-  if (!session?.user) {
-    redirect(`/sign-in?next=/chat/${params.id}`)
-  }
-
-  const chat = await getChat(params.id, session.user.id)
-
-  if (!chat) {
-    notFound()
-  }
-
-  if (chat?.userId !== session?.user?.id) {
-    notFound()
-  }
-
-  return <Chat id={chat.id} initialMessages={chat.messages} />
+  return <Chat id={params.id} initialMessages={[]} />
 }
