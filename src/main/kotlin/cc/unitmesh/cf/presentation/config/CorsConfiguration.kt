34bf6c900d9373108e2248d33b0fbb5d4256203b
Phodal Huang <h@phodal.com>
2023-09-04 17:03:25 +0800

    refactor: split deploy modules

--- src/main/kotlin/cc/unitmesh/cf/presentation/config/CorsConfiguration.kt
+++ src/main/kotlin/cc/unitmesh/cf/presentation/config/CorsConfiguration.kt
@@ -1,10 +1,9 @@
 package cc.unitmesh.cf.presentation.config
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 @EnableWebMvc
