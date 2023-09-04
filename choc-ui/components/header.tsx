e1586de8231fbf999a6dbf17d0aaea0b5578e823
Phodal Huang <h@phodal.com>
2023-09-04 18:44:47 +0800

    docs: update logos

--- choc-ui/components/header.tsx
+++ choc-ui/components/header.tsx
@@ -10,8 +10,7 @@ export async function Header() {
     <header className="sticky top-0 z-50 flex items-center justify-between w-full h-16 px-4 border-b shrink-0 bg-gradient-to-b from-background/10 via-background/50 to-background/80 backdrop-blur-xl">
       <div className="flex items-center">
         <Link href="/" target="_blank" rel="nofollow">
-          <IconNextChat className="w-6 h-6 mr-2 dark:hidden" inverted />
-          <IconNextChat className="hidden w-6 h-6 mr-2 dark:block" />
+          Chocolate Factory
         </Link>
       </div>
       <div className="flex items-center justify-end space-x-2">
