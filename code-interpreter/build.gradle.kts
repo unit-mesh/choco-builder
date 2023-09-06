647e6349ec4def2b0c32fafe33dec28628d37088
Phodal Huang <h@phodal.com>
2023-09-06 20:13:17 +0800

    build: init code interpreter

---
+++ code-interpreter/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.jvm)
+    alias(libs.plugins.serialization)
+    id("org.jetbrains.kotlin.jupyter.api") version "0.12.0-56"
+}
+
+repositories {
+    mavenCentral()
+    maven(url = uri("https://packages.jetbrains.team/maven/p/ktls/maven"))
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+
+    implementation("org.jetbrains.kotlinx:kotlin-jupyter-api:0.12.0-56")
+    implementation("org.jetbrains.kotlinx:kotlin-jupyter-kernel:0.12.0-56")
+
+    // Serverless Kotlin Language Binding
+    implementation(libs.kotless.lang)
+    implementation(libs.kotless.lang.local)
+
+    // Serverless Kotlin Spring
+    implementation(libs.kotless.spring.boot.lang)
+    implementation(libs.kotless.spring.boot.lang.local)
+    implementation(libs.kotless.spring.lang.parser)
+
+    // Serverless Kotlin Ktor
+    implementation(libs.kotless.ktor.lang)
+    implementation(libs.kotless.ktor.lang.local)
+
+    implementation("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.4.2")
+
+    // tips: don't add follow deps to project will cause issues
+    compileOnly("org.jetbrains.kotlin:kotlin-scripting-jvm")
+
+    testImplementation(libs.bundles.test)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 rootProject.name = "chocolate-factory"
 
 include(":cocoa-core")
+include(":code-interpreter")
 include(":cocoa-local-embedding")
 
 dependencyResolutionManagement {
