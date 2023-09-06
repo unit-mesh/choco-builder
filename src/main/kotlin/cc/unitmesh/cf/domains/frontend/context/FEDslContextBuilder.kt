688f05fe8c553a44c0045a0a86d70d3409c770cc
Phodal Huang <h@phodal.com>
2023-09-06 15:22:33 +0800

    docs: update for context

--- src/main/kotlin/cc/unitmesh/cf/domains/frontend/context/FEDslContextBuilder.kt
+++ src/main/kotlin/cc/unitmesh/cf/domains/frontend/context/FEDslContextBuilder.kt
@@ -3,7 +3,6 @@ package cc.unitmesh.cf.domains.frontend.context
 import cc.unitmesh.cf.core.context.DslContext
 import cc.unitmesh.cf.core.context.DslContextBuilder
 import cc.unitmesh.cf.core.dsl.InterpreterContext
-import cc.unitmesh.cf.domains.frontend.context.FrontendDslContext
 import cc.unitmesh.cf.infrastructure.cache.CachedEmbedding
 import cc.unitmesh.cf.infrastructure.similarity.JaccardSimilarity
 import org.springframework.stereotype.Component
