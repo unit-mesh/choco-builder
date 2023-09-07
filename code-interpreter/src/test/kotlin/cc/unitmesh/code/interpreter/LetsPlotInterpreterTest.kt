ce7212ee89a9bebe0826a31af93f7a653d0ebf94
Phodal Huang <h@phodal.com>
2023-09-07 11:52:36 +0800

    test: init basic plot test

--- code-interpreter/src/test/kotlin/cc/unitmesh/code/interpreter/LetsPlotInterpreterTest.kt
+++ code-interpreter/src/test/kotlin/cc/unitmesh/code/interpreter/LetsPlotInterpreterTest.kt
@@ -1,6 +1,13 @@
 package cc.unitmesh.code.interpreter
 
 import cc.unitmesh.code.interpreter.compiler.KotlinReplWrapper
+import org.jetbrains.kotlinx.jupyter.api.HTML
+import org.jetbrains.letsPlot.Stat
+import org.jetbrains.letsPlot.export.ggsave
+import org.jetbrains.letsPlot.geom.geomBar
+import org.jetbrains.letsPlot.geom.geomText
+import org.jetbrains.letsPlot.label.ggtitle
+import org.jetbrains.letsPlot.letsPlot
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -10,6 +17,21 @@ class LetsPlotInterpreterTest {
     @BeforeEach
     internal fun setUp() {
         this.compiler = KotlinReplWrapper()
+    }
+
+    @Test
+    internal fun local_lets_plot() {
+        val incomeData = mapOf(
+            "x" to listOf("一月", "二月", "三月", "四月", "五月", "六月"),
+            "y" to listOf(201.2, 222, 234.3, 120.2, 90, 94.4)
+        )
+        val plot = letsPlot(incomeData) { x = "x"; y = "y" } +
+                geomBar(stat = Stat.identity) +
+                geomText(labelFormat = "\${.2f}") { label = "y"; } +
+                ggtitle("2023 年上半年电费")
+
+
+//        ggsave(plot, "test.png")
     }
 
     @Test
