ea4e15d9d254f2cbe586be1bfd7200c4bc5d1f06
Phodal Huang <h@phodal.com>
2023-09-08 20:05:30 +0800

    fix: fix permissions

--- cocoa-core/src/main/kotlin/cc/unitmesh/cf/core/flow/DomainDeclaration.kt
+++ cocoa-core/src/main/kotlin/cc/unitmesh/cf/core/flow/DomainDeclaration.kt
@@ -8,9 +8,3 @@ interface DomainDeclaration {
     val description: String
     fun workflow(question: String): Workflow
 }
-
-/**
- * 子域用于根据用户的问题，定位到对应的子领域，如：用户想编写一个前端页面，那么子域就是前端页面。
- */
-interface SubDomainDeclaration {
-}
--- src/main/kotlin/cc/unitmesh/cf/domains/DomainClassify.kt
+++ src/main/kotlin/cc/unitmesh/cf/domains/DomainClassify.kt
@@ -3,6 +3,7 @@ package cc.unitmesh.cf.domains
 import cc.unitmesh.cf.core.flow.DomainDeclaration
 import cc.unitmesh.cf.infrastructure.cache.CachedEmbeddingService
 import org.reflections.Reflections
+import org.reflections.util.ConfigurationBuilder
 import org.springframework.stereotype.Component
 
 
@@ -17,7 +18,7 @@ class DomainClassify(private val cachedEmbedding: CachedEmbeddingService) {
             return cachedDomains
         }
 
-        Reflections(packageName)
+        Reflections(ConfigurationBuilder().forPackages(packageName))
             .getSubTypesOf(DomainDeclaration::class.java)
             .map {
                 val newInstance = Class.forName(it.name).getDeclaredConstructor().newInstance() as DomainDeclaration
