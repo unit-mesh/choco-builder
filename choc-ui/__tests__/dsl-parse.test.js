a85e3c1ea5188da1e34c140d68cd9297f41fa82b
Phodal Huang <h@phodal.com>
2023-09-11 21:46:59 +0800

    test(frontend): init test framework

---
+++ choc-ui/__tests__/dsl-parse.test.js
@@ -0,0 +1,5 @@
+describe('DSL Parser', () => {
+  it('should render the heading', () => {
+    expect(1).toBe(1)
+  })
+})
---
+++ choc-ui/components/dsl/design-dsl.ts
@@ -0,0 +1,86 @@
+interface DesignInformation {
+  projectConfigs: Record<string, string>
+  flows: DFlow[]
+  components: Record<string, DComponent>
+  layouts: DLayout[]
+  libraries: DLibrary[]
+}
+
+interface DConfig {
+  key: string
+  value: string
+}
+
+interface DComponent {
+  name: string
+  child?: DComponent[]
+  configs?: Record<string, string>
+}
+
+interface DSee {
+  componentName: string
+  data: string
+}
+
+interface DDo {
+  uiEvent: string
+  componentName: string
+  data: string
+}
+
+interface DReact {
+  sceneName: string
+  reactAction: string
+  reactComponentName: string
+  animateName: string
+  reactComponentData: string
+}
+
+interface DInteraction {
+  see: DSee
+  do: DDo
+  react: DReact[]
+}
+
+interface DFlow {
+  interactions: DInteraction[]
+  flowName: string
+}
+
+interface DLayout {
+  layoutName: string
+  layoutRows: DLayoutRow[]
+}
+
+interface DLayoutRow {
+  layoutCells: DLayoutCell[]
+}
+
+interface DLayoutCell {
+  componentName: string
+  layoutInformation: string
+  normalInformation: string
+}
+
+interface DProperty {
+  key: string
+  value: string
+}
+
+interface LibraryPreset {
+  key: string
+  value: string
+  presetCalls?: PresetCall[]
+  subProperties?: DProperty[]
+  inheritProps?: DProperty[]
+}
+
+interface PresetCall {
+  name: string
+  preset: string
+}
+
+interface DLibrary {
+  name: string
+  presets: LibraryPreset[]
+}
