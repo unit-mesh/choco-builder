073ada8a56f3cfbd39917ddb3183f8ad7763daf5
Phodal Huang <h@phodal.com>
2023-09-14 08:14:51 +0800

    test: test embdedding

--- cocoa-core/src/main/kotlin/cc/unitmesh/nlp/embedding/Embedding.kt
+++ cocoa-core/src/main/kotlin/cc/unitmesh/nlp/embedding/Embedding.kt
@@ -3,6 +3,10 @@ package cc.unitmesh.nlp.embedding
 // I don't know why, but sometimes the following import is not added automatically.
 public typealias Embedding = List<Double>
 
-fun toEmbedding(text: MutableList<Float>): Embedding {
+fun toEmbedding(text: Iterable<Float>): Embedding {
+    return text.map { it.toDouble() }
+}
+
+fun toEmbedding(text: FloatArray): Embedding {
     return text.map { it.toDouble() }
 }
---
+++ cocoa-core/src/test/kotlin/cc/unitmesh/rag/store/InMemoryEmbeddingStoreTest.kt
@@ -0,0 +1,37 @@
+package cc.unitmesh.rag.store;
+
+import cc.unitmesh.nlp.embedding.Embedding
+import cc.unitmesh.nlp.embedding.toEmbedding
+import cc.unitmesh.rag.document.Document
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class InMemoryEmbeddingStoreTest {
+
+    @Test
+    fun should_add_embedding_with_generated_id() {
+        val embeddingStore: EmbeddingStore<Document> = InMemoryEmbeddingStore()
+
+        val embedding1: Embedding = toEmbedding(floatArrayOf(1f, 3f))
+        val segment1 = Document.from("first")
+        embeddingStore.add(embedding1, segment1)
+
+        val embedding2: Embedding = toEmbedding(floatArrayOf(2f, 2f))
+        val segment2 = Document.from("second")
+        val id2: String = embeddingStore.add(embedding2, segment2)
+
+        val embedding3: Embedding = toEmbedding(floatArrayOf(3f, 1f))
+        val segment3 = Document.from("third")
+        val id3: String = embeddingStore.add(embedding3, segment3)
+
+        val relevant: List<EmbeddingMatch<Document>> =
+            embeddingStore.findRelevant(toEmbedding(floatArrayOf(4f, 0f)), 2)
+
+        Assertions.assertThat(relevant).containsExactly(
+            EmbeddingMatch(0.9743416490252569, id3, embedding3, segment3),
+            EmbeddingMatch(0.8535533905932737, id2, embedding2, segment2)
+        )
+    }
+
+}
