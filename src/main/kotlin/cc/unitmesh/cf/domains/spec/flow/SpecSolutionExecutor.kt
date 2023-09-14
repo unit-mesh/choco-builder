8fecb077d6eb16881723073435b34580cc21f4cd
Phodal Huang <h@phodal.com>
2023-09-14 20:46:52 +0800

    refactor(spec): remove unused vars

--- src/main/kotlin/cc/unitmesh/cf/domains/spec/flow/SpecSolutionExecutor.kt
+++ src/main/kotlin/cc/unitmesh/cf/domains/spec/flow/SpecSolutionExecutor.kt
@@ -1,16 +1,25 @@
 package cc.unitmesh.cf.domains.spec.flow
 
+import cc.unitmesh.cf.core.dsl.Dsl
+import cc.unitmesh.cf.core.dsl.DslInterpreter
 import cc.unitmesh.cf.core.dsl.Interpreter
 import cc.unitmesh.cf.core.flow.SolutionExecutor
 import cc.unitmesh.cf.core.flow.model.Answer
 import cc.unitmesh.cf.core.llms.LlmProvider
-import cc.unitmesh.cf.domains.spec.context.SpecVariableResolver
-import cc.unitmesh.cf.domains.spec.model.SpecLang
 import io.reactivex.rxjava3.core.Flowable
 
+
+data class SpecLang(
+    val words: List<String>,
+    override val content: String,
+) : Dsl {
+    override var domain: String = "spec"
+    override var interpreters: List<DslInterpreter> = listOf()
+}
+
+// TODO: add support for solution executor
 class SpecSolutionExecutor(
-    private val completion: LlmProvider,
-    private val variable: SpecVariableResolver,
+    private val completion: LlmProvider
 ): SolutionExecutor<SpecLang> {
     override val interpreters: List<Interpreter> = listOf()
 
