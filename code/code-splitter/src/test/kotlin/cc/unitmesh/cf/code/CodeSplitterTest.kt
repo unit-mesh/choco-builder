bd7b3eb8d6b0a1589a6c54fbec60c75545e18e3a
Phodal Huang <h@phodal.com>
2023-09-16 17:14:43 +0800

    fix: fix tsts

--- code/code-splitter/src/test/kotlin/cc/unitmesh/cf/code/CodeSplitterTest.kt
+++ code/code-splitter/src/test/kotlin/cc/unitmesh/cf/code/CodeSplitterTest.kt
@@ -2,17 +2,21 @@ package cc.unitmesh.cf.code;
 
 import chapi.domain.core.CodeDataStruct
 import chapi.domain.core.CodeFunction
+import io.kotest.matchers.shouldBe
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class CodeSplitterTest {
 
     @Test
-    fun should_splitCodeDataStructIntoDocuments() {
+    fun should_use_ds_content() {
         // Given
+        val content =
+            "class MyClass {\n    fun foo() {\n        println(\"Hello, World!\")\n    }\n    fun bar() {\n        println(\"Goodbye, World!\")\n    }\n}"
         val codeDataStruct = CodeDataStruct(
             Package = "com.example",
             NodeName = "MyClass",
+            Content = content,
             Functions = listOf(
                 CodeFunction(
                     Content = "fun foo() {\n    println(\"Hello, World!\")\n}",
@@ -29,9 +33,8 @@ class CodeSplitterTest {
         val documents = codeSplitter.split(codeDataStruct)
 
         // Then
-        assertThat(documents).hasSize(2)
-        assertThat(documents[0].text).isEqualTo("// canonicalName: com.example.MyClass\nfun foo() {\n    println(\"Hello, World!\")\n}")
-        assertThat(documents[1].text).isEqualTo("// canonicalName: com.example.MyClass\nfun bar() {\n    println(\"Goodbye, World!\")\n}")
+        assertThat(documents).hasSize(1)
+        documents[0].text shouldBe content
     }
 
     @Test
