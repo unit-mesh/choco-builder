e66799aa78c1901d0cd9252638f1d39492bbb4c2
Phodal Huang <h@phodal.com>
2023-09-19 10:52:22 +0800

    refactor: split kotlin module

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,13 +1,87 @@
-@Suppress("DSL_SCOPE_VIOLATION")
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
+
+    kotlin("plugin.spring") version "1.9.10"
+    kotlin("plugin.jpa") version "1.9.10"
+
+    id("org.springframework.boot") version "3.1.3"
+    id("io.spring.dependency-management") version "1.1.3"
+    id("org.jetbrains.kotlinx.dataframe") version "0.11.1"
+
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
 }
 
+// allow scripts to unpack, when spring boot start, those packages will unpack to some dir, so we can call it REPL.
+tasks.withType<BootJar> {
+    requiresUnpack("**/kotlin-compiler-*.jar")
+    requiresUnpack("**/kotlin-script-*.jar")
+    requiresUnpack("**/kotlin-scripting-*.jar")
+    requiresUnpack("**/kotlin-jupyter-*.jar")
+    requiresUnpack("**/lets-plot-*.jar")
+    requiresUnpack("**/dataframe-*.jar")
+    requiresUnpack("**/kotlinx-*.jar")
+}
+
+
 dependencies {
-    implementation(libs.kotlin.stdlib)
+    implementation(projects.llmModules.sentenceTransformers)
+    implementation(projects.cocoaCore)
+    implementation(projects.dsl.design)
+    implementation(projects.code.interpreter)
+    implementation(projects.code.codeSplitter)
+    implementation(projects.ragModules.storeElasticsearch)
+
+    // kotlin dependencies
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation(libs.serialization.json)
     implementation(libs.coroutines.core)
 
+    implementation("io.reactivex.rxjava3:rxjava:3.1.7")
+
+    // spring dependencies
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
+    // spring dev dependencies
+//    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // clone git repo
+    compileOnly("org.archguard.codedb:action-toolkit:0.1.2")
+    implementation("org.archguard.codedb:checkout:0.1.2")
+    implementation(libs.chapi.domain)
+
+    // llm dependencies
+    implementation(libs.azure.openai)
+    implementation(libs.bundles.openai)
+    implementation(libs.huggingface.tokenizers)
+
+    // data serialization dependencies
+    implementation(libs.bundles.jackson)
+
+    // data convert
+    implementation(kotlin("reflect"))
+    implementation(libs.reflections)
+    implementation(libs.dataframe)
+
+    // database dependencies
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    implementation(libs.velocity.engine)
+
+    // load config from .env file for testing
+    testImplementation(libs.dotenv.kotlin)
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation(libs.bundles.test)
     testRuntimeOnly(libs.test.junit.engine)
 }
