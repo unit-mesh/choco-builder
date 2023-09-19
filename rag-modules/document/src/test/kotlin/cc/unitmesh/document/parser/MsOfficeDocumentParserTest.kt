cfe69b6fed83e1998cf37afe1fa0c7cfcdeefcea
Phodal Huang <h@phodal.com>
2023-09-19 20:48:13 +0800

    test: add test for sample

--- rag-modules/document/src/test/kotlin/cc/unitmesh/document/parser/MsOfficeDocumentParserTest.kt
+++ rag-modules/document/src/test/kotlin/cc/unitmesh/document/parser/MsOfficeDocumentParserTest.kt
@@ -1,2 +1,14 @@
 package cc.unitmesh.document.parser;
 
+import cc.unitmesh.rag.document.DocumentType
+import org.junit.jupiter.api.Test
+
+class MsOfficeDocumentParserTest  {
+    @Test
+    fun should_parse_pptx_file() {
+        val inputStream = this.javaClass.getResourceAsStream("/sample.pptx")
+        val document = MsOfficeDocumentParser(DocumentType.PPT).parse(inputStream)
+
+        assert(document.text.contains("代码库问答"))
+    }
+}
