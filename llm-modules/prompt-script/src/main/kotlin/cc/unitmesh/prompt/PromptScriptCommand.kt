f2479fce445a7959ac33881a484576de70eb2f60
Phodal Huang <h@phodal.com>
2023-10-01 15:29:31 +0800

    refactor: fix gen module issue

--- llm-modules/prompt-script/src/main/kotlin/cc/unitmesh/prompt/PromptScriptCommand.kt
+++ llm-modules/prompt-script/src/main/kotlin/cc/unitmesh/prompt/PromptScriptCommand.kt
@@ -4,6 +4,8 @@ import cc.unitmesh.prompt.executor.ScriptExecutor
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import org.slf4j.Logger
+import java.io.File
 
 val HELP_TEXT = """Run custom prompt script for AI Workflow PoC""".trimIndent()
 
@@ -11,28 +13,28 @@ class PromptScriptCommand : CliktCommand(help = HELP_TEXT) {
     /**
      * Input file should be a yaml file.
      */
-    val input by option(help = "input file").required()
+    val config by option(help = "prompt script config file").required()
 
     companion object {
-        val logger = org.slf4j.LoggerFactory.getLogger(PromptScriptCommand::class.java)
+        val logger: Logger = org.slf4j.LoggerFactory.getLogger(PromptScriptCommand::class.java)
     }
 
     override fun run() {
         // check is yaml file
-        if (!input.endsWith(".yaml") && !input.endsWith(".yml")) {
-            throw Exception("input file should be a yaml file: $input")
+        if (!config.endsWith(".yaml") && !config.endsWith(".yml")) {
+            throw Exception("input file should be a yaml file: $config")
         }
 
         // check input file exits
-        val file = java.io.File(input)
+        val file = File(config)
         if (!file.exists()) {
-            throw Exception("input file not found: $input")
+            throw Exception("input file not found: $config")
         }
 
         // execute script
         val executor = ScriptExecutor(file)
         executor.execute()
 
-        logger.debug("execute script success: $input")
+        logger.debug("execute script success: $config")
     }
 }
