f747cc7817f89e28204b10b9b9110c5930404f3e
Phodal Huang <h@phodal.com>
2023-10-02 13:44:15 +0800

    fix: fix tests

--- llm-modules/prompt-script/src/test/kotlin/cc/unitmesh/prompt/model/VariableTest.kt
+++ llm-modules/prompt-script/src/test/kotlin/cc/unitmesh/prompt/model/VariableTest.kt
@@ -2,13 +2,14 @@ package cc.unitmesh.prompt.model;
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 
 class VariableTest {
 
     @Test
     fun should_return_range_with_integer_step() {
         val range = Variable.Range("key", "0~100", "1")
-        val result = range.toRange()
-        assertEquals(0.0..100.0, result)
+        val result: ClosedRange<BigDecimal> = range.toRange()
+        assertEquals(0.toBigDecimal().rangeTo(100.toBigDecimal()), result)
     }
 }
