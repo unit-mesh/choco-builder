fba2fe03c6bb92beb1a677e9d96935e9baba0fad
Phodal Huang <h@phodal.com>
2023-10-07 20:36:48 +0800

    refactor: extract interface to parrent class

--- docs-builder/src/main/kotlin/cc/unitmesh/docs/KDocGen.kt
+++ docs-builder/src/main/kotlin/cc/unitmesh/docs/KDocGen.kt
@@ -1,18 +1,14 @@
 package cc.unitmesh.docs
 
+import cc.unitmesh.docs.kdoc.findKDoc
 import cc.unitmesh.docs.model.DocContent
 import cc.unitmesh.docs.model.DocGenerator
 import cc.unitmesh.docs.model.RootDocContent
-import cc.unitmesh.docs.sample.ClassSample
-import cc.unitmesh.docs.sample.FunctionSample
-import cc.unitmesh.docs.kdoc.findKDoc
 import com.intellij.lang.ASTNode
 import com.intellij.lang.FileASTNode
 import com.pinterest.ktlint.KtFileProcessor
 import org.jetbrains.kotlin.KtNodeTypes
 import org.jetbrains.kotlin.psi.KtClass
-import org.jetbrains.kotlin.psi.KtElement
-import org.jetbrains.kotlin.psi.KtFunction
 import org.jetbrains.kotlin.psi.psiUtil.children
 import java.nio.file.Path
 
@@ -20,7 +16,6 @@ import java.nio.file.Path
 class KDocGen(private val rootDir: Path) : DocGenerator() {
     private val processor: KtFileProcessor = KtFileProcessor()
     private var inheritanceDoc = mutableMapOf<String, List<DocContent>>()
-    private var classNodes = listOf<KtClass>()
     private var fileNodes = listOf<FileASTNode>()
 
     override fun execute(): List<RootDocContent> {
@@ -103,78 +98,6 @@ class KDocGen(private val rootDir: Path) : DocGenerator() {
         }
 
         return docs
-    }
-
-    fun buildSample(node: KtElement): ClassSample? {
-        val testClass = classNodes.find { it.name == "${node.name}Test" || it.name == "${node.name}Tests" }
-        if (testClass == null) {
-            return null
-        }
-
-        val classBody = testClass.body ?: return null
-        val methods: List<KtFunction> = classBody.node.children()
-            .filter { it.elementType == KtNodeTypes.FUN }
-            .map { it.psi as KtFunction }
-            .filter {
-                it.modifierList?.text?.contains("@SampleCode") ?: false
-            }
-            .toList()
-
-        // the code block will begin with `// start-sample` and end with `// end-sample`
-        val sourceCodes = methods.mapNotNull { method ->
-            val sampleCodeValue = getSampleCodeValue(method)
-
-            val lines = method.text.split("\n")
-            // find the start line with regex
-            val startLine = lines.indexOfFirst { it.contains("// start-sample") }
-            // find the end line with regex
-            val endLine = lines.indexOfFirst { it.contains("// end-sample") }
-            // get the code block
-            if (startLine == -1 || endLine == -1) {
-                null
-            } else {
-                val linesResults = lines.subList(startLine + 1, endLine).joinToString("\n") {
-                    it.replaceFirst(Regex("^\\s+"), "")
-                }
-
-                sampleCodeValue.code = linesResults
-                sampleCodeValue
-            }
-        }
-
-        return ClassSample(node.name!!, sourceCodes)
-    }
-
-    private fun getSampleCodeValue(method: KtFunction): FunctionSample {
-        // get modifier value from `@SampleCode(name = "检验成功", content = "")`
-        var name = ""
-        var description = ""
-        method.modifierList?.annotationEntries?.filter {
-            it.shortName?.asString() == "SampleCode"
-        }?.mapNotNull { annotation ->
-            annotation.valueArguments.forEach { valueArgument ->
-                val astNode = valueArgument.asElement().node
-                astNode.children().forEach { node ->
-                    when (node.elementType) {
-                        KtNodeTypes.VALUE_ARGUMENT_NAME -> {
-                            when (node.text) {
-                                "name" -> {
-                                    val byType = astNode.findChildByType(KtNodeTypes.STRING_TEMPLATE)
-                                    name = byType?.text.orEmpty().removeSurrounding("\"")
-                                }
-
-                                "content" -> {
-                                    val contentType = astNode.findChildByType(KtNodeTypes.STRING_TEMPLATE)
-                                    description = contentType?.text.orEmpty().removeSurrounding("\"")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return FunctionSample(name, description)
     }
 }
 
--- docs-builder/src/main/kotlin/cc/unitmesh/docs/model/DocGenerator.kt
+++ docs-builder/src/main/kotlin/cc/unitmesh/docs/model/DocGenerator.kt
@@ -1,8 +1,87 @@
 package cc.unitmesh.docs.model
 
+import cc.unitmesh.docs.sample.ClassSample
+import cc.unitmesh.docs.sample.FunctionSample
+import org.jetbrains.kotlin.KtNodeTypes
+import org.jetbrains.kotlin.psi.KtClass
+import org.jetbrains.kotlin.psi.KtElement
+import org.jetbrains.kotlin.psi.KtFunction
+import org.jetbrains.kotlin.psi.psiUtil.children
+
 abstract class DocGenerator {
+    var classNodes = listOf<KtClass>()
 
     open fun execute(): List<RootDocContent> {
         return listOf()
+    }
+
+    fun buildSample(node: KtElement): ClassSample? {
+        val testClass =
+            classNodes.find { it.name == "${node.name}Test" || it.name == "${node.name}Tests" } ?: return null
+
+        val classBody = testClass.body ?: return null
+        val methods: List<KtFunction> = classBody.node.children()
+            .filter { it.elementType == KtNodeTypes.FUN }
+            .map { it.psi as KtFunction }
+            .filter {
+                it.modifierList?.text?.contains("@SampleCode") ?: false
+            }
+            .toList()
+
+        // the code block will begin with `// start-sample` and end with `// end-sample`
+        val sourceCodes = methods.mapNotNull { method ->
+            val sampleCodeValue = getSampleCodeValue(method)
+
+            val lines = method.text.split("\n")
+            // find the start line with regex
+            val startLine = lines.indexOfFirst { it.contains("// start-sample") }
+            // find the end line with regex
+            val endLine = lines.indexOfFirst { it.contains("// end-sample") }
+            // get the code block
+            if (startLine == -1 || endLine == -1) {
+                null
+            } else {
+                val linesResults = lines.subList(startLine + 1, endLine).joinToString("\n") {
+                    it.replaceFirst(Regex("^\\s+"), "")
+                }
+
+                sampleCodeValue.code = linesResults
+                sampleCodeValue
+            }
+        }
+
+        return ClassSample(node.name!!, sourceCodes)
+    }
+
+    private fun getSampleCodeValue(method: KtFunction): FunctionSample {
+        // get modifier value from `@SampleCode(name = "检验成功", content = "")`
+        var name = ""
+        var description = ""
+        method.modifierList?.annotationEntries?.filter {
+            it.shortName?.asString() == "SampleCode"
+        }?.mapNotNull { annotation ->
+            annotation.valueArguments.forEach { valueArgument ->
+                val astNode = valueArgument.asElement().node
+                astNode.children().forEach { node ->
+                    when (node.elementType) {
+                        KtNodeTypes.VALUE_ARGUMENT_NAME -> {
+                            when (node.text) {
+                                "name" -> {
+                                    val byType = astNode.findChildByType(KtNodeTypes.STRING_TEMPLATE)
+                                    name = byType?.text.orEmpty().removeSurrounding("\"")
+                                }
+
+                                "content" -> {
+                                    val contentType = astNode.findChildByType(KtNodeTypes.STRING_TEMPLATE)
+                                    description = contentType?.text.orEmpty().removeSurrounding("\"")
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return FunctionSample(name, description)
     }
 }
