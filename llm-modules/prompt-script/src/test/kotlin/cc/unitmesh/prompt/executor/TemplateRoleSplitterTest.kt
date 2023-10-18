61027be2b85fc7aaf32c454f7d0a9760faaa0a12
Phodal Huang <h@phodal.com>
2023-10-18 11:33:34 +0800

    fix: fix tests

--- llm-modules/prompt-script/src/test/kotlin/cc/unitmesh/prompt/executor/TemplateRoleSplitterTest.kt
+++ llm-modules/prompt-script/src/test/kotlin/cc/unitmesh/prompt/executor/TemplateRoleSplitterTest.kt
@@ -23,6 +23,38 @@ class TemplateRoleSplitterTest {
 
 
     @Test
+    fun should_splitInputIntoSections() {
+        // given
+        val input = "```system```\nYou are a helpful assistant.\n\n```user```\n${'$'}{question}\n"
+        val expectedSections = mapOf(
+            "system" to "You are a helpful assistant.\n\n",
+            "user" to "${'$'}{question}\n\n"
+        )
+
+        // when
+        val sections = TemplateRoleSplitter().split(input)
+
+        // then
+        assertEquals(expectedSections, sections)
+    }
+
+    @Test
+    fun should_handleRemainingContentAfterLastSection() {
+        // given
+        val input = "```system```\nYou are a helpful assistant.\n\n```user```\n${'$'}{question}\nRemaining content"
+        val expectedSections = mapOf(
+            "system" to "You are a helpful assistant.\n\n",
+            "user" to "${'$'}{question}\nRemaining content\n"
+        )
+
+        // when
+        val sections = TemplateRoleSplitter().split(input)
+
+        // then
+        assertEquals(expectedSections, sections)
+    }
+
+    @Test
     fun `should split input into sections`() {
         // given
         val input = """
