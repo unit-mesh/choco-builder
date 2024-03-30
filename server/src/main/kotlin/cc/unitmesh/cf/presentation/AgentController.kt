c3b6faf9ab5bbccc7c00ba695cedcc5937defd35
Phodal Huang <h@phodal.com>
2024-03-31 05:55:15 +0800

    feat(presentation): refactor AgentController to use a new explain-biz endpoint for providing business logic explanations.

--- server/src/main/kotlin/cc/unitmesh/cf/presentation/AgentController.kt
+++ server/src/main/kotlin/cc/unitmesh/cf/presentation/AgentController.kt
@@ -1,17 +1,13 @@
 package cc.unitmesh.cf.presentation
 
-import cc.unitmesh.cf.domains.DomainClassify
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/agents")
-class AgentController(val classify: DomainClassify) {
-    companion object {
-        private val log = org.slf4j.LoggerFactory.getLogger(AgentController::class.java)
-    }
-
-    @PostMapping("/smart")
-    fun smartAgent(@RequestParam("q") query: String): String {
+@RequestMapping("/agent")
+class AgentController {
+    @PostMapping("/explain-biz")
+    // todo: align to OpenAPI format and return Flow<String>
+    fun explainBiz(): String {
         // 1. find problem domain
         // 2. match problem domain to solution domain
         // 3. find solution domain
