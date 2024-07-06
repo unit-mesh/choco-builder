769a2585c48b65af8dba2ad6816684364f8602b0
Phodal Huang <h@phodal.com>
2024-07-06 22:50:51 +0800

    refactor(embedding): improve OpenAiEncoding encode and decode methods
    Refactored the encode method to use IntArrayList for better performance and modified the decode method to handle IntArrayList instead of List<Int>.

--- cocoa-core/src/main/kotlin/cc/unitmesh/nlp/embedding/OpenAiEncoding.kt
+++ cocoa-core/src/main/kotlin/cc/unitmesh/nlp/embedding/OpenAiEncoding.kt
@@ -4,15 +4,20 @@ import com.knuddels.jtokkit.Encodings
 import com.knuddels.jtokkit.api.Encoding
 import com.knuddels.jtokkit.api.EncodingRegistry
 import com.knuddels.jtokkit.api.EncodingType
+import com.knuddels.jtokkit.api.IntArrayList
 
 class OpenAiEncoding : EncodingTokenizer {
     private val registry: EncodingRegistry = Encodings.newLazyEncodingRegistry()
     private val encoding: Encoding = registry.getEncoding(EncodingType.CL100K_BASE)
+
     override fun encode(text: String): List<Int> {
-        return encoding.encode(text)
+        val encode: IntArrayList = encoding.encode(text)
+        return encode.boxed()
     }
 
     override fun decode(tokens: List<Int>): String {
-        return encoding.decode(tokens)
+        val intArray: IntArrayList = IntArrayList(tokens.size)
+        tokens.forEach { intArray.add(it) }
+        return encoding.decode(intArray)
     }
 }
